1f51af21bcd7d0070d89c04884ca1cd971107ab6
p0ppy-fl0w3r <kunai_madara@protonmail.com>
2021-12-23 15:42:01 +0545

    Added sorting.

--- Charts/WeeklyReport.Designer.cs
+++ Charts/WeeklyReport.Designer.cs
@@ -55,6 +55,7 @@
             this.weeklyDataChart.Size = new System.Drawing.Size(420, 224);
             this.weeklyDataChart.TabIndex = 0;
             this.weeklyDataChart.Text = "Weekly Report Chart";
+            this.weeklyDataChart.DoubleClick += new System.EventHandler(this.chartDoubleClick);
             // 
             // weeklyDataTable
             // 
@@ -62,7 +63,7 @@
             this.weeklyDataTable.ColumnCount = 3;
             this.weeklyDataTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.49502F));
             this.weeklyDataTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.50498F));
-            this.weeklyDataTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 116F));
+            this.weeklyDataTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 118F));
             this.weeklyDataTable.Location = new System.Drawing.Point(12, 287);
             this.weeklyDataTable.Name = "weeklyDataTable";
             this.weeklyDataTable.RowCount = 9;
@@ -83,7 +84,8 @@
             this.sortCombo.FormattingEnabled = true;
             this.sortCombo.Items.AddRange(new object[] {
             "Number Of Visitors",
-            "Income"});
+            "Income",
+            "Name of Day"});
             this.sortCombo.Location = new System.Drawing.Point(311, 247);
             this.sortCombo.Name = "sortCombo";
             this.sortCombo.Size = new System.Drawing.Size(121, 21);
